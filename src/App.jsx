af03e2421fbf4688ea659f3a89639f43f10207dd
Alona Tiunina <163348743+tiunina@users.noreply.github.com>
2024-10-23 23:32:17 +0300

    first submition

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage/HomePage';
+import MoviesPage from './pages/MoviesPage/MoviesPage';
+import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import Navigation from './components/Navigation/Navigation';
+import MovieCast from './components/MovieCast/MovieCast';
+import MovieReviews from './components/MovieReviews/MovieReviews';
+function App() {
+  return (
+    <header>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </header>
+  );
+}
+
+export default App;
